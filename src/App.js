2fd14a1e6c560a29c246473af19b6401398835db
deepakrajtech <technicaldeepakraj@gmail.com>
2024-09-15 08:52:19 +0530

    Add Footer Link Deepak Raj Tech

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import './index.css'
 function App() {
   return (
     <>
+    <header className="header">
+      <a href="https://deepakrajtech.com" className="header-link">
+      DEEPAKRAJTech
+      </a>
+      <p className="powered-by">Powered by DEEPAKRAJTech</p>
+    </header>
       {data.map((val, index) => <Card key={index} text={val.text} author={val.author} />)}
     </>
   );
